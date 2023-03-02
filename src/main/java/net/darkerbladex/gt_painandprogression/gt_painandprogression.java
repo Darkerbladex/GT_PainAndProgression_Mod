262f009fe5785ed43204ef0f6124b918b2ad4981
Darker BladeX <darkerbladex0@gmail.com>
2023-03-01 16:08:53 -0800

    Initial commit

---
+++ src/main/java/net/darkerbladex/gt_painandprogression/gt_painandprogression.java
@@ -0,0 +1,31 @@
+package net.darkerbladex.gt_painandprogression;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = gt_painandprogression.MOD_D, name = gt_painandprogression.NAME, version = gt_painandprogression.VERSION)
+public class gt_painandprogression
+{
+    public static final String MOD_D = "gt_painandprogression";
+    public static final String NAME = "GT:Pain&ProgressionMod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
